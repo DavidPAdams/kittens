83271f99959af19ef1e16666a5796b2f51d6e4d3
David Adams <david.riding.rails@gmail.com>
2019-03-19 15:52:26 -0700

    Update kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,13 +1,40 @@
 class KittensController < ApplicationController
+
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
   def index
+    @kittens = Kitten.all
   end
 
   def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    
+  end
+
+  def update
+    
   end
 
   def edit
   end
 
   def show
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
   end
 end
